d2c981b538bd6e5d4982e60deed63cce7d203f8c
jamesxsc <james@xsc.co.uk>
2024-10-18 11:13:02 +0100

    testbench task1

--- task1/counter.sv
+++ task1/counter.sv
@@ -1,5 +1,5 @@
 module counter #(
-    parameter WIDTH = 8;
+    parameter WIDTH = 8
 )(
     // interface signals
     input   logic               clk,    // clock
@@ -10,6 +10,6 @@ module counter #(
 
 always_ff @ (posedge clk)
     if (rst)    count <= {WIDTH{1'b0}}; // non-blocking assignment, since we are in always_ff block
-    else        count <= count + {{WIDTH-1{1'b0}}, en};
+    else        count <= count + {{WIDTH-1{1'b0}}, en}; // construct concats 7 bits of 0 and either 1 or 0 depending on if en is high which is added to count
     
 endmodule
---
+++ task1/counter_tb.cpp
@@ -0,0 +1,39 @@
+#include "Vcounter.h" // generated source
+#include "verilated.h"
+#include "verilated_vcd_c.h" // to generate trace
+
+int main(int argc, char **argv, char **env) {
+    int i;
+    int clk;
+
+    // init 
+    Verilated::commandArgs(argc, argv);
+
+    Vcounter* top = new VCounter;
+
+    Verilated::traceEverOn(true); // init trace dump
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace(tfp, 99);
+    tfp->open("counter.vcd");
+
+    // initialize inputs to DUT
+    top->clk = 1;
+    top->rst = 1;
+    top->en = 0;
+
+    // run simulation
+    for (i = 0; i < 300; i++) {
+        // dump variables to trace
+        for (clk = 0; clk < 2; clk++) {
+            tfp->dump(2 * i * clk);
+            top->clk = !top->clk;
+            top->eval();
+        }
+        top->rst = (i < 2 || i == 15); // times to reset at
+        top->en = (i > 4);
+        if (Verilated::gotFinish()) exit(0);
+    }
+
+    tfp->close();
+    exit(0);
+}
